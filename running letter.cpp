c94ae6237effeb2c812ef0af4e178b0008469ed3
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-10-07 19:39:48 +0800

    Add files via upload

---
+++ running letter.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include<windows.h>
+const int l = 0;
+const int r=120;
+int main()
+{   int a = 0, b = 1;
+	while (1) {
+		a += b;
+		if (a == l || a == r) { b = -b; }
+		for (int i = 1; i <= a; ++i) { putchar(' '); }
+		putchar('o');
+		Sleep(50);
+		system("cls");
+	}
+	return 0;
+}
