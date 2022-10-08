4993f5d126a72cd090c0013569bda4b75d56fb79
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-10-08 08:09:37 +0800

    Add files via upload

---
+++ encrypt _decrypt.cpp
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+void encrypt() {
+	char code;
+	do {
+		code = getchar();
+		if (code != '\n') {
+			if (code >= 'a' && code < 'z'
+				|| code >= 'A' && code < 'Z'
+				|| code >= '0' && code < '9') {
+				putchar(++code);
+			}
+			else if (code == 'z' || code == 'Z') {
+				putchar(code -= 25);
+			}
+			else if (code == '9') { putchar('0'); }
+			else putchar(code);
+		}
+	} while (code != '\n');
+}
+void decrypt() {
+	char code;
+	do {
+		code = getchar();
+		if (code != '\n') {
+			if (code > 'a' && code <= 'z'
+				|| code > 'A' && code <= 'Z'
+				|| code > '0' && code <= '9') {
+				putchar(--code);
+			}
+			else if (code == 'a' || code == 'A') {
+				putchar(code -= 25);
+			}
+			else if (code == '0') { putchar('9'); }
+			else putchar(code);
+		}
+	} while (code != '\n');
+}
+int main() {
+	printf("1.encrypt 2.decrypt\n");
+	char judge[3]={};
+	gets_s(judge,3);
+	if (judge[0] == '1') { encrypt(); }
+	if (judge[0] == '2') { decrypt(); }
+	return 0;
+}
