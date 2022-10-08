fe90fd103e713988ef754998b2646ff76b5f6b4e
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-10-08 13:01:28 +0800

    Add files via upload

--- isPrime.cpp
+++ isPrime.cpp
@@ -2,12 +2,12 @@
 #include<time.h>
 int main()
 {
-	int num,i=0,c=0;
-	clock_t time,s,e;
+	int num, i = 0, c = 0;
+	clock_t time, s, e;
 	scanf_s("%d", &num);
 	s = clock();
 	if (num == 2) { ++c; printf("y "); }
-	else if (num % 2 == 0) { c += 2;; printf("n "); }
+	else if (num % 2 == 0 || num == 1) { c += 2;; printf("n "); }
 	else {
 		c += 2;
 		for (i = 3; i * i <= num; i += 2) {
@@ -22,6 +22,6 @@ int main()
 	}
 	e = clock();
 	time = e - s;
-	printf("%ld %d",time,c);
+	printf("%ld %d", time, c);
 	return 0;
 }
