72b5907e5000874fd4681c595f5f84eeffe27012
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-12-24 10:15:52 +0800

    Add files via upload

---
+++ p04_narcissus.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+    int one, ten, hundred;
+    for (int num = 100; num < 1000; ++num) {
+        one = num / 100;
+        ten = (num - one * 100) / 10;
+        hundred = num - one * 100 - ten * 10;
+        if (one * one * one + ten * ten * ten + hundred * hundred * hundred == num)
+        {
+            printf("%d ", num);
+        }
+    }
+    return 0;
+}
+
