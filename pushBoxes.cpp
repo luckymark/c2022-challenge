097ec9b029434d0d672bd2b7a166d1f2f62bd063
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-12-24 09:10:21 +0800

    Add files via upload

--- pushBoxes.cpp
+++ pushBoxes.cpp
@@ -1,150 +1,194 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 #include<conio.h>
 #include<windows.h>
+const int ROW = 7, QUEUE = 10;//include'\n' and NULL
+char ar[ROW][QUEUE] = { 0 };
+void map(char MAP[100]);
+int play(void);
+void savegoal(int Goal);
+char MAP1[100] = "C:\\Users\\86134\\source\\repos\\box\\map1.txt";
+char MAP2[100] = "C:\\Users\\86134\\source\\repos\\box\\map2.txt";
 int main() {
-	char ar[11][11] = {
-		"0000000000",
-		"0000000000",
-		"0000000000",
-		"000&000000",
-		"000@000000",
-		"000#*  000",
-		"000 $  000",
-		"000  00000",
-		"0000000000",
-		"0000000000",
-		"0000000000"
-	};
-	char cha;
-	int x = 5, y = 3, a = 4, b = 3, A = 5, B = 4, C = 6, D = 4, c = 3, d = 3, i, flag = 0;
-	for (i = 0; i < 11;++i) {
-		puts(ar[i]);
+	map(MAP1);
+	int Goal=play();
+	savegoal(Goal);
+	printf("\nNext?\n1.yes 2.no\n");
+	int choose = 2;
+	scanf_s("%d", &choose);
+	if (choose == 1) {
+		map(MAP2);
+		Goal=play();
+		savegoal(Goal);
+		return 0;
+	}else{ return 0; }
+}
+void savegoal(int Goal) {
+	FILE* fp;
+	fopen_s(&fp, "C:\\Users\\86134\\source\\repos\\box\\goal.txt", "a");
+	if (fp == NULL) {
+		printf("fail\n");
+		system("pause");
+		exit(-1);
 	}
-	printf("*->@ $->&\nPlease use wasd to move");
+	fprintf(fp, "%d\n", Goal);
+	fclose(fp);
+}
+void map(char MAP[100]) {
+	FILE* fp;
+	fopen_s(&fp, MAP, "r");
+	if (fp == NULL) {
+		printf("fail\n");
+		system("pause");
+		exit(-1);
+	}
+	for (int i = 0; i < ROW; ++i) {
+		fgets(ar[i], QUEUE, fp);
+	}
+	fclose(fp);
+}
+int play(void) {
+	int x = 0, y = 0, A = 0, B = 0, C = 0, D = 0, a = 0, b = 0, c = 0, d = 0;//where to start(x,y) where is the boxes(A,B C,D) where is the goals(a,b c,d) 
+	for (int i = 0; i < ROW; ++i) {
+		for (int j = 0; j < QUEUE; ++j) {
+			if (ar[i][j] == '#') { x = i; y = j; }
+			if (ar[i][j] == '*') { A = i; B = j; }
+			if (ar[i][j] == '@') { a = i; b = j; }
+			if (ar[i][j] == '$') { C = i; D = j; }
+			if (ar[i][j] == '&') { c = i; d = j; }
+		}
+	}
+	system("cls");
+	int  i;
+	for (i = 0; i < ROW; ++i) {
+		printf("%s", ar[i]);
+	}
+	printf("\n*->@ $->&\nPlease use W A S D to move");
+	char cha;
+	int step = 0;
 	while (A != a || B != b || C != c || D != d) {
 		cha = _getch();
 		if (cha == 's') {
-			if (ar[x + 1][y] == ' '||ar[x+1][y]=='@'||ar[x+1][y]=='&') {
+			if (ar[x + 1][y] == ' ' || ar[x + 1][y] == '@' || ar[x + 1][y] == '&') {
 				ar[x][y] = ' ';
 				ar[++x][y] = '#';
-				++flag;
+				++step;
 			}
-			else if (ar[x+2][y ] != '0') {
-			     if ((ar[x + 1][y] == '*' || ar[x + 1][y] == '$') && (ar[x + 2][y] == '*' || ar[x + 2][y] == '$') && (ar[x + 3][y] != '0')) {
-				    ar[C += 1][D] = '$';
-				    ar[A += 1][B] = '*';
-				    ar[x][y] = ' ';
-				    ar[++x][y] = '#';
-				++flag;
-			}
-				else if (ar[x+1][y ] == '*' && ar[x+2][y ] != '$') {
-					ar[A+=1][B ] = '*';
+			else if (ar[x + 2][y] != '0') {
+				if ((ar[x + 1][y] == '*' || ar[x + 1][y] == '$') && (ar[x + 2][y] == '*' || ar[x + 2][y] == '$') && (ar[x + 3][y] != '0')) {
+					ar[C += 1][D] = '$';
+					ar[A += 1][B] = '*';
 					ar[x][y] = ' ';
 					ar[++x][y] = '#';
-					++flag;
+					++step;
 				}
-				else if (ar[x+1][y ] == '$' && ar[x+2][y ] != '*') {
-					ar[C+=1][D ] = '$';
+				else if (ar[x + 1][y] == '*' && ar[x + 2][y] != '$') {
+					ar[A += 1][B] = '*';
 					ar[x][y] = ' ';
 					ar[++x][y] = '#';
-					++flag;
+					++step;
+				}
+				else if (ar[x + 1][y] == '$' && ar[x + 2][y] != '*') {
+					ar[C += 1][D] = '$';
+					ar[x][y] = ' ';
+					ar[++x][y] = '#';
+					++step;
 				}
 			}
 		}
-		if (cha == 'w' ) {
+		if (cha == 'w') {
 			if (ar[x - 1][y] == ' ' || ar[x - 1][y] == '@' || ar[x - 1][y] == '&') {
 				ar[x][y] = ' ';
 				ar[--x][y] = '#';
-				++flag;
+				++step;
 			}
 			else if (ar[x - 2][y] != '0') {
-			 if ((ar[x - 3][y] != '0')&&(ar[x - 1][y] == '*' || ar[x - 1][y] == '$') && (ar[x - 2][y] == '*' || ar[x - 2][y] == '$')) {
-				ar[C -= 1][D] = '$';
-				ar[A -= 1][B] = '*';
-				ar[x][y] = ' ';
-				ar[--x][y] = '#';
-				++flag;
-			}
-			 else if (ar[x - 1][y] == '*' && ar[x-2][y ] != '$') {
+				if ((ar[x - 3][y] != '0') && (ar[x - 1][y] == '*' || ar[x - 1][y] == '$') && (ar[x - 2][y] == '*' || ar[x - 2][y] == '$')) {
+					ar[C -= 1][D] = '$';
 					ar[A -= 1][B] = '*';
 					ar[x][y] = ' ';
 					ar[--x][y] = '#';
-					++flag;
+					++step;
 				}
-				else if (ar[x - 1][y] == '$' && ar[x-2][y ] != '*') {
+				else if (ar[x - 1][y] == '*' && ar[x - 2][y] != '$') {
+					ar[A -= 1][B] = '*';
+					ar[x][y] = ' ';
+					ar[--x][y] = '#';
+					++step;
+				}
+				else if (ar[x - 1][y] == '$' && ar[x - 2][y] != '*') {
 					ar[C -= 1][D] = '$';
 					ar[x][y] = ' ';
 					ar[--x][y] = '#';
-					++flag;
+					++step;
 				}
 			}
 		}
 		if (cha == 'd' && ar[x][y + 1] != '0') {
-				if (ar[x][y + 1] == ' ' || ar[x][y + 1] == '@' || ar[x][y + 1] == '&') {
+			if (ar[x][y + 1] == ' ' || ar[x][y + 1] == '@' || ar[x][y + 1] == '&') {
+				ar[x][y] = ' ';
+				ar[x][++y] = '#';
+				++step;
+			}
+			else if (ar[x][y + 2] != '0') {
+				if ((ar[x][y + 3] != '0') && (ar[x][y + 1] == '*' || ar[x][y + 1] == '$') && (ar[x][y + 2] == '*' || ar[x][y + 2] == '$')) {
+					ar[C][D += 1] = '$';
+					ar[A][B += 1] = '*';
 					ar[x][y] = ' ';
 					ar[x][++y] = '#';
-					++flag;
-				}	
-				else if (ar[x][y + 2] != '0') {
-						if ((ar[x][y + 3] != '0')&&(ar[x][y + 1] == '*' || ar[x][y + 1] == '$') && (ar[x][y + 2] == '*' || ar[x][y + 2] == '$')) {
-							ar[C][D += 1] = '$';
-							ar[A][B += 1] = '*';
-							ar[x][y] = ' ';
-							ar[x][++y] = '#';
-							++flag;
-						}
-			        else if (ar[x][y + 1] == '*'&&ar[x][y+2]!='$') {
-					    ar[A][B += 1] = '*';
-					    ar[x][y] = ' ';
-					    ar[x][++y] = '#';
-					    ++flag;
-				    }
-				    else if (ar[x][y + 1] == '$'&&ar[x][y+2]!='*') {
-					    ar[C][D += 1] = '$';
-					    ar[x][y] = ' ';
-					    ar[x][++y] = '#';
-					    ++flag;
-				    }
-			    }   
+					++step;
+				}
+				else if (ar[x][y + 1] == '*' && ar[x][y + 2] != '$') {
+					ar[A][B += 1] = '*';
+					ar[x][y] = ' ';
+					ar[x][++y] = '#';
+					++step;
+				}
+				else if (ar[x][y + 1] == '$' && ar[x][y + 2] != '*') {
+					ar[C][D += 1] = '$';
+					ar[x][y] = ' ';
+					ar[x][++y] = '#';
+					++step;
+				}
+			}
 		}
 		if (cha == 'a' && ar[x][y - 1] != '0') {
 			if (ar[x][y - 1] == ' ' || ar[x][y - 1] == '@' || ar[x][y - 1] == '&') {
 				ar[x][y] = ' ';
 				ar[x][--y] = '#';
-				++flag;
+				++step;
 			}
 			else if (ar[x][y - 2] != '0') {
-				if ((ar[x][y - 1] == '*' || ar[x][y - 1] == '$') && (ar[x][y - 2] == '*' || ar[x][y - 2] == '$')&&(ar[x][y-3]!='0')) {
+				if ((ar[x][y - 1] == '*' || ar[x][y - 1] == '$') && (ar[x][y - 2] == '*' || ar[x][y - 2] == '$') && (ar[x][y - 3] != '0')) {
 					ar[C][D -= 1] = '$';
 					ar[A][B -= 1] = '*';
 					ar[x][y] = ' ';
 					ar[x][--y] = '#';
-					++flag;
+					++step;
 				}
 				else if (ar[x][y - 1] == '*' && ar[x][y - 2] != '$') {
 					ar[A][B -= 1] = '*';
 					ar[x][y] = ' ';
 					ar[x][--y] = '#';
-					++flag;
+					++step;
 				}
 				else if (ar[x][y - 1] == '$' && ar[x][y - 2] != '*') {
 					ar[C][D -= 1] = '$';
 					ar[x][y] = ' ';
 					ar[x][--y] = '#';
-					++flag;
+					++step;
 				}
 			}
 		}
 		system("cls");
-		if (ar[4][3] != '#'&& ar[4][3] != '$' && ar[4][3] != '*') { ar[4][3] = '@'; }
-		if (ar[3][3] != '#' && ar[3][3] != '$' && ar[3][3] != '*') { ar[3][3] = '&'; }
-		for (i = 0; i < 11; ++i) {
-			puts(ar[i]);
+		if (ar[a][b] != '#' && ar[a][b] != '$' && ar[a][b] != '*') { ar[a][b] = '@'; }
+		if (ar[c][d] != '#' && ar[c][d] != '$' && ar[c][d] != '*') { ar[c][d] = '&'; }
+		for (i = 0; i < ROW; ++i) {
+			printf("%s", ar[i]);//incllude'\n'
 		}
-		Sleep(100);
+		Sleep(50);
 	}
-	printf("You win!The score is %d", 122-flag);
-	return 0;
+	int fullgoal = 100,goal;
+	printf("\nYou win!The score is %d", goal=fullgoal - step);
+	return goal;
 }
-//int*p=()malloc(100*sizeof());free(p)
