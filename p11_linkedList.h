e7f1dd461e69abc888ec1094fba058daa9ead5d4
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2022-12-25 10:52:34 +0800

    Add files via upload

--- p11_linkedList.h
+++ p11_linkedList.h
@@ -1,12 +1,19 @@
 #pragma once
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 struct Node {
 	int data;
-	Node* next;
+	struct Node* next;
 };
 Node* createList(void);
 void printList(Node* head);
 Node* swapList(Node* head);
 int searchFirstNode(Node* head, int num);
 int searchNextNode(Node* head, int num, int last);
+Node* insertNode(Node*head,Node*node,int order,const char*way);
+Node* insertAhead(Node* head, Node* node, int order);
+Node* insertBehind(Node* head, Node* node, int order);
+Node* insertTail(Node*head,Node*node);
+Node*deleteNode(Node*head,int order);
+void deleteTail(Node*head);
--- p11_linkedList_function.cpp
+++ p11_linkedList_function.cpp
@@ -72,3 +72,86 @@ int searchNextNode(Node* head, int num, int last) {
 	}
 	else { return -1; }
 }
+Node* insertNode(Node* head,Node*node, int order, const char* way)
+{
+	int ret = -1;
+	if (strcmp(way, "ahead") == 0) { ret = 1; }
+	else if (strcmp(way, "behind") == 0) { ret = 2; }
+	switch (ret) {
+	case 1:head = insertAhead(head,node,order); break;
+	case 2:insertBehind(head,node,order); break;
+	default:printf("error"); system("puase"); return NULL; break;
+	}
+	return head;
+}
+Node*insertAhead(Node*head,Node*node,int order){
+	Node* headNode = head;
+	int flag = 1;
+	if (order == 1) { node->next = head; return node; }
+	else while (head->next != NULL) {
+		++flag;
+		if (flag == order) { node->next = head->next; head->next = node; return headNode; }
+		head = head->next;
+	}
+	printf("error"); system("pause"); return headNode;
+}
+Node*insertBehind(Node*head,Node*node,int order){
+	Node* headNode = head;
+	int flag = 1;
+	while (head!=NULL) {
+		if (flag == order) {
+			node->next = head->next;
+			head->next = node;
+			return headNode;
+		}
+		head = head->next;
+		++flag;
+	}
+	printf("error"); system("pause");
+	return headNode;
+}
+Node* insertTail(Node* head, Node* node) {
+	Node* headNode = head;
+	while (1) {
+			if (head->next == NULL) { head->next = node; return headNode; }
+			head = head->next;
+	}
+}
+Node* deleteNode(Node*head,int order){
+	Node* headNode = head;
+	int flag = 1;
+	if (order == 1) {
+		if (head->next != NULL) {
+			headNode = head->next;
+			free(head);
+			return headNode;
+		}
+		else { printf("empty"); system("pause"); return NULL; }
+	}
+	else while (head->next!=NULL) {
+		++flag;
+		if (order == flag) {
+			Node* deleted = head->next;
+			head->next = head->next->next;
+			free(deleted);
+			return headNode;
+		}
+		head = head->next;
+	}
+	printf("error"); system("pause");
+	return headNode;
+}
+void deleteTail(Node* head) {
+	Node* node = head, * last = NULL;
+	if (head->next != NULL) {
+		while (node->next != NULL) {
+			last = node;
+			node = node->next;
+		}
+		last->next = NULL;
+		free(node);
+	}
+	else {
+		printf("empty"); system("pause"); free(head);
+	}
+}
