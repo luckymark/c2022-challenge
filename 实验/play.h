70637215271e83a84cfb81ca8d713ea1b54ef32a
furuoshan <108115675+furuoshan@users.noreply.github.com>
2023-02-20 11:05:28 +0800

    Create play.h

---
+++ 实验/play.h
@@ -0,0 +1,9 @@
+#ifndef GOBANG_LAST_EDITION_PLAY_H
+#define GOBANG_LAST_EDITION_PLAY_H
+
+#endif //GOBANG_LAST_EDITION_PLAY_H
+
+#include "setScore.h"
+
+Node *playAI(short x,short y); //输入玩家的最后一步
+bool cut(int x,int y,Node *node);
